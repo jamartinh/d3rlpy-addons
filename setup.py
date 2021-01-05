049305fcae8bf6076676f8780850a5a08c0e7db7
ja.martin.h@repsol.com <ja.martin.h@repsol.com>
2021-01-05 03:07:52 +0100

    First commit simple.py EpisodicFitter Generator interface

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='d3rlpy-addons',
+    version='0.1',
+    packages=[
+        'd3rlpy_addons', 'd3rlpy_addons.fitters', 'd3rlpy_addons.wrappers'
+    ],
+    url='',
+    license='MIT',
+    author='Jose Antonio Martin H.',
+    author_email='xjamartinh@gmail.com',
+    description='Addons for d3rpy RL library',
+    install_requires=[
+        "torch", "scikit-learn", "tqdm", "h5py", "gym", "d3rlpy"
+    ],
+)
