ffb02ec227d49546337f4934956d64a963e48d11
R107333 <ja.martin.h@repsol.com>
2021-07-02 09:30:24 +0200

    Added DMeanQFunctionFactory

--- d3rlpy_addons/models/q_functions.py
+++ d3rlpy_addons/models/q_functions.py
@@ -2,8 +2,47 @@ from typing import Any, ClassVar, Dict
 
 from d3rlpy.models.q_functions import QFunctionFactory, register_q_func_factory
 from d3rlpy.models.torch import Encoder, EncoderWithAction
-from d3rlpy_addons.models.torch import ContinuousDQRQFunction
-from d3rlpy_addons.models.torch.q_functions import DiscreteDQRQFunction
+from d3rlpy_addons.models.torch.q_functions import ContinuousDMeanQFunction, ContinuousDQRQFunction, DiscreteDMeanQFunction, DiscreteDQRQFunction
+
+
+class DMeanQFunctionFactory(QFunctionFactory):
+    """Differential Regression Mean Q function factory class.
+           It is a little trick to set all q_values to an initial fixed value so that
+           optimistic, pessimistic or arbitrary q_value initialization can be set.
+
+        References:
+            * unpublished: Jose Antonio Martin H.
+
+        Args:
+            q_value_offset: the initial q_values for the nn approximation
+
+        """
+
+    TYPE: ClassVar[str] = "dmean"
+    _q_value_offset: float
+
+    def __init__(self, bootstrap: bool = False, share_encoder: bool = False, q_value_offset: float = 0):
+        super().__init__(bootstrap, share_encoder)
+        self._q_value_offset = q_value_offset
+
+    def create_discrete(
+            self,
+            encoder: Encoder,
+            action_size: int,
+    ) -> DiscreteDMeanQFunction:
+        return DiscreteDMeanQFunction(encoder, action_size, self._q_value_offset)
+
+    def create_continuous(
+            self,
+            encoder: EncoderWithAction,
+    ) -> ContinuousDMeanQFunction:
+        return ContinuousDMeanQFunction(encoder, self._q_value_offset)
+
+    def get_params(self, deep: bool = False) -> Dict[str, Any]:
+        return {
+            "bootstrap": self._bootstrap,
+            "share_encoder": self._share_encoder,
+        }
 
 
 class DQRQFunctionFactory(QFunctionFactory):
@@ -24,7 +63,8 @@ class DQRQFunctionFactory(QFunctionFactory):
     _n_quantiles: int
     _q_value_offset: float
 
-    def __init__(self, n_quantiles: int = 32, q_value_offset: float = 0):
+    def __init__(self, bootstrap=False, share_encoder=False, n_quantiles: int = 32, q_value_offset: float = 0):
+        super().__init__(bootstrap, share_encoder)
         self._n_quantiles = n_quantiles
         self._q_value_offset = q_value_offset
 
@@ -66,3 +106,4 @@ class DQRQFunctionFactory(QFunctionFactory):
 
 
 register_q_func_factory(DQRQFunctionFactory)
+register_q_func_factory(DMeanQFunctionFactory)
--- d3rlpy_addons/models/torch/q_functions.py
+++ d3rlpy_addons/models/torch/q_functions.py
@@ -1,9 +1,11 @@
 from typing import cast
 
 import torch
-from d3rlpy.models.torch import Encoder, EncoderWithAction
-from d3rlpy.models.torch.q_functions.qr_q_function import ContinuousQRQFunction, DiscreteQRQFunction
 from torch import nn
+
+from d3rlpy.models.torch import Encoder, EncoderWithAction
+from d3rlpy.models.torch.q_functions.mean_q_function import ContinuousMeanQFunction, DiscreteMeanQFunction
+from d3rlpy.models.torch.q_functions.qr_q_function import ContinuousQRQFunction, DiscreteQRQFunction
 
 
 class DiscreteDQRQFunction(DiscreteQRQFunction):
@@ -64,3 +66,51 @@ class ContinuousDQRQFunction(ContinuousQRQFunction):
             self, h: torch.Tensor, taus: torch.Tensor
     ) -> torch.Tensor:
         return cast(torch.Tensor, (self._fc(h) - self._fc0(h)) + self._q_value_offset)
+
+
+class DiscreteDMeanQFunction(DiscreteMeanQFunction):  # type: ignore
+    _fc0: nn.Linear
+    _q_value_offset: float
+
+    def __init__(self, encoder: Encoder, action_size: int, q_value_offset: float = 0.0):
+        super().__init__(encoder=encoder, action_size=action_size)
+
+        # initial q_values for approximation
+        self._q_value_offset = q_value_offset
+
+        # get a new instance or clone a frozen copy
+        self._fc0 = type(self._fc)(encoder.get_feature_size())
+
+        # copy weights and stuff
+        self._fc0.load_state_dict(self._fc.state_dict())
+
+        # freeze model by freezing parameters
+        for param in self._fc0.parameters():
+            param.requires_grad = False
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return cast(torch.Tensor, self._fc(self._encoder(x)) - self._fc0(self._encoder(x)) + self._q_value_offset)
+
+
+class ContinuousDMeanQFunction(ContinuousMeanQFunction):  # type: ignore
+    _fc0: nn.Linear
+    _q_value_offset: float
+
+    def __init__(self, encoder: EncoderWithAction, q_value_offset: float = 0.0):
+        super().__init__(encoder=encoder)
+
+        # initial q_values for approximation
+        self._q_value_offset = q_value_offset
+
+        # get a new instance or clone a frozen copy
+        self._fc0 = type(self._fc)(encoder.get_feature_size())
+
+        # copy weights and stuff
+        self._fc0.load_state_dict(self._fc.state_dict())
+
+        # freeze model by freezing parameters
+        for param in self._fc0.parameters():
+            param.requires_grad = False
+
+    def forward(self, x: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        return cast(torch.Tensor, self._fc(self._encoder(x, action)) - self._fc0(self._encoder(x, action)) + self._q_value_offset)
