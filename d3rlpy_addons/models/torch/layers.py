275e94285ceb6f462025966eb6baeee1beda3c1d
R107333 <ja.martin.h@repsol.com>
2021-11-10 12:09:58 +0100

    noise layers.py

--- d3rlpy_addons/models/torch/layers.py
+++ d3rlpy_addons/models/torch/layers.py
@@ -17,22 +17,25 @@ class GaussianNoiseInverseWeightLayer(nn.Module):
         self.std = std
 
     def forward(self, obs):
-        obs_repeated = torch.repeat_interleave(
-            obs, repeats=self.n_samples, dim=0
+
+        return gaussian_noise_inverse_weight(
+            obs, self.n_samples, self.mean, self.std
         )
-        noise_t = torch.normal(self.mean, self.std, size=obs_repeated.size()).to('cuda')
-        noise_t[0 : obs.shape[0]] = 0.0
-        obs_noise = obs_repeated + noise_t
-
-        # Compute euclidean distance
-        d = ((obs_repeated - obs_noise) ** 2).sum(axis=1)
-        d = d.view(self.n_samples, obs.shape[0])
-
-        w = 1 / (1 + d)
-        w = torch.softmax(w, dim=0)
-        w -= w.min()
-        w /= w.max()
-        w = w.view(obs_noise.shape[0], 1)
-        return obs_noise, w
 
 
+def gaussian_noise_inverse_weight(obs, n_samples, mean, std):
+    obs_repeated = torch.repeat_interleave(obs, repeats=n_samples, dim=0)
+    noise_t = torch.normal(mean, std, size=obs_repeated.size()).to("cuda")
+    noise_t[0 : obs.shape[0]] = 0.0
+    obs_noise = obs_repeated + noise_t
+
+    # Compute euclidean distance
+    d = ((obs_repeated - obs_noise) ** 2).sum(axis=1)
+    d = d.view(n_samples, obs.shape[0])
+
+    w = 1 / (1 + d)
+    w = torch.softmax(w, dim=0)
+    w -= w.min()
+    w /= w.max()
+    w = w.view(obs_noise.shape[0], 1)
+    return obs_noise, w
